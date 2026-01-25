b398af83c21dae4675c4aee0edbeac7468983b8a
Luke Rieff <luke.rieff@gmail.com>
2026-01-25 15:46:02 +0100

    Add keycodes config param

--- config.py
+++ config.py
@@ -1,12 +1,29 @@
 import configparser
 from pathlib import Path
 
+
+def parse_key_codes(param: str) -> dict[int, str]:
+    """
+    Parse the specified keycodes parameter.
+    :param param: The parameter.
+    :return: The parsed keycodes and their associated labels.
+    """
+
+    # Split the param into pairs.
+    pairs: list[list[str]] = [segment.split(':', 2) for segment in param.split(',')]
+    if any(len(pair) < 2 for pair in pairs):
+        raise Exception(f'Invalid keycodes, must consist of key-value pairs.')
+
+    # Turn the pairs into a dict.
+    return dict([(int(pair[0]), pair[1]) for pair in pairs])
+
+
 # Read the configuration.
 config = configparser.ConfigParser()
 config.read(['/etc/camera-connect/config.ini', 'config.ini'])
 
 # Trigger configuration.
-KEY_CODE: int = config.getint('Trigger', 'KeyCode')
+KEY_CODES: dict[int, str] = parse_key_codes(config.get('Trigger', 'KeyCodes'))
 GRAB: bool = config.getboolean('Trigger', 'Grab')
 
 # General configuration.
